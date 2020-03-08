b6a8972db831ebc93332103dca25f6dac323f02e
Jack Martinez <jm5833@nyu.edu>
2020-03-08 18:05:05 -0400

    Client has been updated so that it can connect to the server

--- NFS_client.go
+++ NFS_client.go
@@ -4,13 +4,25 @@ import (
     "strings"
     "bufio"
     "os"
+    "net"
+    "log"
 )
+
+var server net.Conn
+var err error
+var port string = ":1337"
+
 func main() {
+    fmt.Println("Attempting to connect to NFS...")
+    server,err = net.Dial("tcp", port)
+    if err != nil{
+        log.Fatal("Cannot connect to NFS!")
+    }
     reader := bufio.NewReader(os.Stdin)
     for{
-        fmt.Print("Client->: ")
+        fmt.Print("Client-> ")
         command,_ := reader.ReadString('\n')
         command = strings.Replace(command, "\n", "", -1)
-        fmt.Println(command)
+        server.Write([]byte(command))
     }
 }
