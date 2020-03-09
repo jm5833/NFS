7d94f16a8460b9e2fad5231021925435782df9a4
Jack Martinez <jm5833@nyu.edu>
2020-03-08 20:14:01 -0400

    Updated client so that it can read messages from server

--- NFS_client.go
+++ NFS_client.go
@@ -1,7 +1,7 @@
 package main
 import (
     "fmt"
-    "strings"
+    //"strings"
     "bufio"
     "os"
     "net"
@@ -12,17 +12,41 @@ var server net.Conn
 var err error
 var port string = ":1337"
 
-func main() {
+//function to display messages from server
+func handleServerMessage(server net.Conn){
+    readscan := bufio.NewScanner(server)
+    for readscan.Scan(){
+        fmt.Println(readscan.Text())
+    }
+}
+
+//function to accept input from stdin
+//to send over to server
+func acceptInput(server net.Conn){
+    reader := bufio.NewReader(os.Stdin)
+    go handleServerMessage(server)
+    for{
+        fmt.Print("Client-> ")
+        command,_ := reader.ReadString('\n')
+        //command = strings.Replace(command, "\n", "", -1)
+        server.Write([]byte(command))
+    }
+
+}
+
+//function to connect to the server
+//returns a net.Conn object that's for the server
+func serverConnect() net.Conn{
     fmt.Println("Attempting to connect to NFS...")
     server,err = net.Dial("tcp", port)
     if err != nil{
         log.Fatal("Cannot connect to NFS!")
     }
-    reader := bufio.NewReader(os.Stdin)
-    for{
-        fmt.Print("Client-> ")
-        command,_ := reader.ReadString('\n')
-        command = strings.Replace(command, "\n", "", -1)
-        server.Write([]byte(command))
-    }
+    fmt.Println("Connected!")
+    return server
+}
+
+func main() {
+    server = serverConnect()
+    acceptInput(server)
 }
